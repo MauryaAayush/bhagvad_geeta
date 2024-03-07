07e969847981f49c04dcf3b9bff9aac51dd552f9
MauryaAayush <maayush235@gmail.com>
2024-03-08 00:23:36 +0530

    Radhey Radhey

---
+++ lib/Views/SelectionScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SelectedScreen extends StatefulWidget {
+  const SelectedScreen({super.key});
+
+  @override
+  State<SelectedScreen> createState() => _SelectedScreenState();
+}
+
+class _SelectedScreenState extends State<SelectedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Views/SelectionScreen.dart';
+import 'Views/mainscreen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Color(0xFFFDB211)));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'श्रीमद भगवत गीता',
+      debugShowCheckedModeBanner: false,
+      // title: 'श्रीमद भगवत गीता',
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: Scaffold(
-        body: Container(),
-      ),
+     initialRoute: '/',
+      routes: {
+        '/' : (context) => const MainScreen(),
+        '/second' : (context) => const SelectedScreen(),
+      },
     );
   }
 }
-
-
