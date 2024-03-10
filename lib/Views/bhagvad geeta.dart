55c44d8fd1cd5ede3eccf9838cb90574a54de039
Admin <maayush235@gmail.com>
2024-03-10 18:30:59 +0530

    Radhey Radhey

--- lib/Views/bhagvad geeta.dart
+++ lib/Views/bhagvad geeta.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/List.dart';
@@ -12,7 +13,6 @@ class Bhagvad_geeta extends StatefulWidget {
 class _Bhagvad_geetaState extends State<Bhagvad_geeta> {
   @override
   Widget build(BuildContext context) {
-
     int index = 0;
 
     double height = MediaQuery.of(context).size.height;
@@ -23,95 +23,90 @@ class _Bhagvad_geetaState extends State<Bhagvad_geeta> {
       body: Stack(
         children: [
           Container(
-            height: height*0.6,
+            height: height * 0.6,
             width: width,
             decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/images/appBG.png'),
-                  fit: BoxFit.cover,
-                )),
+              image: AssetImage('assets/images/appBG.png'),
+              fit: BoxFit.cover,
+            )),
           ),
-
-          Container(
-            width: width,
-            // color: Colors.blue,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.s,
-              children: [
-                Container(
-                  margin:
-                  EdgeInsets.fromLTRB(0, height * 0.07, 0, height * 0.02),
-                  height: height * 0.25,
-                  width: width * 0.53,
-                  // color: Colors.red,
-                  child: Image.asset(
-                    'assets/images/Geeta_logo.png',
-                    fit: BoxFit.cover,
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              width: width,
+              // color: Colors.blue,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.s,
+                children: [
+                  Container(
+                    margin:
+                        EdgeInsets.fromLTRB(0, height * 0.07, 0, height * 0.02),
+                    height: height * 0.25,
+                    width: width * 0.53,
+                    // color: Colors.red,
+                    child: Image.asset(
+                      'assets/images/Geeta_logo.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                Container(
-                  height: height * 0.6,
-                  width: width * 0.85,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFEF2DA),
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  Container(
+                    width: width * 0.85,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFEF2DA),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ...List.generate(data[0]['Bhaags'].length,
+                            (index) => selectContainer(index))
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/first');
-                        },
-                        child: Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: height * 0.009, horizontal: width * 0.055),
-                          padding: EdgeInsets.symmetric(vertical: height*0.008),
-                          height: height * 0.11,
-                          width: width,
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(colors: [
-                                Color(0xFFFDB316),
-                                Color(0xFFFFFC341)
-                              ]),
-                              borderRadius: BorderRadius.all(Radius.circular(5))
-                          ),
-
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                'assets/images/icon1.png',
-                              ),
-                              VerticalDivider(
-                                color: Colors.black26,
-                              ),
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                    data[index]['index'],
-                                    style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-
-
-
-
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           )
+        ],
+      ),
+    );
+  }
 
+  Container selectContainer(int index) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      margin: EdgeInsets.symmetric(
+          vertical: height * 0.009, horizontal: width * 0.055),
+      padding: EdgeInsets.symmetric(vertical: height * 0.008),
+      height: height * 0.11,
+      width: width,
+      decoration: BoxDecoration(
+          gradient:
+              LinearGradient(colors: [Color(0xFFFDB316), Color(0xFFFFFC341)]),
+          borderRadius: BorderRadius.all(Radius.circular(5))),
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/icon1.png',
+          ),
+          VerticalDivider(
+            color: Colors.black26,
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                data[0]['Bhaags'][index]['name'],
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/Views/geeta saar.dart
+++ lib/Views/geeta saar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/List.dart';
+
 class Saar extends StatefulWidget {
   const Saar({super.key});
 
@@ -28,35 +30,112 @@ class _SaarState extends State<Saar> {
                 )),
           ),
 
-          Container(
-            width: width,
-            // color: Colors.blue,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.s,
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: height*0.04),
-                  height: height * 0.3,
-                  width: width * 0.5,
-                  // color: Colors.red,
-                  child: Image.asset(
-                    'assets/images/Geeta_logo.png',
-                    fit: BoxFit.cover,
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+
+              width: width,
+              // color: Colors.blue,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.s,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0,height*0.085,0,0),
+                    height: height * 0.21,
+                    width: width * 0.52,
+                    // color: Colors.red,
+                    child: Image.asset(
+                      'assets/images/Geeta_logo.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                // Container(
-                //
-                //   height: height,
-                //   width: width*0.5,
-                //   color: Colors.red,
-                //
-                // ),
-              ],
+
+                  Container(
+                    // height: height * 0.6,
+                    width: width,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFEF2DA),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: Column(
+                      children: [
+
+                        ...List.generate(data[1]['bhaags'].length, (index) =>  buildContainer(index),)
+
+
+
+
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
             ),
-          )
+          ),
 
         ],
       ),
     );
   }
+
+  Container buildContainer(int index) {
+
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+
+    return Container(
+                        padding: EdgeInsets.all(20),
+                          margin: EdgeInsets.all(20),
+                          // height: height * 0.5,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0xFFFDB316),
+                              borderRadius: BorderRadius.all(Radius.circular(8)),
+                              border: Border(bottom:BorderSide(width: 10))
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                data[1]['bhaags'][index]['id'],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: height / 55,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black,
+                                ),
+                              ),
+
+                              Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Text(
+                                  data[1]['bhaags'][index]['name'],
+                                  style: TextStyle(
+                                    fontSize: height / 40,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+
+
+                              Text(
+                                data[1]['bhaags'][index]['meaning'],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: height / 55,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          )
+
+
+                      );
+  }
+
+
+
 }
