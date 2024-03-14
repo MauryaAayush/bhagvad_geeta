585865246c7e4367aa9acc1b4b66e1ab2ca0e36d
Admin <maayush235@gmail.com>
2024-03-14 13:23:32 +0530

    Radhey Radhey

--- lib/Views/geeta aarti.dart
+++ lib/Views/geeta aarti.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 import '../utils/List.dart';
 
 class Aarti extends StatefulWidget {
@@ -13,7 +12,6 @@ class Aarti extends StatefulWidget {
 class _AartiState extends State<Aarti> {
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
@@ -22,26 +20,25 @@ class _AartiState extends State<Aarti> {
       body: Stack(
         children: [
           Container(
-            height: height*0.6,
+            height: height * 0.6,
             width: width,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/images/appBG.png'),
-                  fit: BoxFit.cover,
-                )),
+              image: AssetImage('assets/images/appBG.png'),
+              fit: BoxFit.cover,
+            )),
           ),
-
           SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Container(
-            height: height*1.5,
+            child: SizedBox(
+              height: height * 1.5,
               width: width,
               // color: Colors.blue,
               child: Column(
                 // mainAxisAlignment: MainAxisAlignment.s,
                 children: [
                   Container(
-                    margin: EdgeInsets.fromLTRB(0,height*0.085,0,0),
+                    margin: EdgeInsets.fromLTRB(0, height * 0.085, 0, 0),
                     height: height * 0.21,
                     width: width * 0.52,
                     // color: Colors.red,
@@ -53,56 +50,47 @@ class _AartiState extends State<Aarti> {
                   Container(
                     height: height * 0.6,
                     width: width,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Color(0xFFFEF2DA),
                       borderRadius: BorderRadius.all(Radius.circular(30)),
                     ),
                     child: Container(
-                      margin: EdgeInsets.all(20),
-                      height: height * 0.5,
-                      width: width * 0.8,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFFDB316),
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        border: Border(bottom:BorderSide(width: 10))
-                      ),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              data[3]['index'],
+                        margin: const EdgeInsets.all(20),
+                        height: height * 0.5,
+                        width: width * 0.8,
+                        decoration: const BoxDecoration(
+                            color: Color(0xFFFDB316),
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            border: Border(bottom: BorderSide(width: 10))),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                data[3]['index'],
+                                style: TextStyle(
+                                  fontSize: height / 40,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              data[3]['aarti'],
+                              textAlign: TextAlign.center,
                               style: TextStyle(
-                                fontSize: height / 40,
-                                fontWeight: FontWeight.w300,
+                                fontSize: height / 55,
+                                fontWeight: FontWeight.w400,
                                 color: Colors.black,
                               ),
                             ),
-                          ),
-
-
-                      Text(
-                        data[3]['aarti'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: height / 55,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                      ),
-                        ],
-                      )
-
-                    ),
+                          ],
+                        )),
                   )
-
-
-
                 ],
               ),
             ),
           )
-
         ],
       ),
     );
